1a1dcccce9ee71dd3d809f0893efcfd0cc5051e6
hershalb <sunil@calvisitor-10-105-189-83.calvisitor.1918.berkeley.edu>
2016-08-24 12:15:30 -0700

    first commit

--- app/App.js
+++ app/App.js
@@ -4,7 +4,7 @@ import {render} from 'react-dom';
 class App extends Component {
   render(){
     return (
-      <h1>Hello World</h1>
+      <h1>Hello World! This is a the start of your new application!</h1>
     );
   }
 }
